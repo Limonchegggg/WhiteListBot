5794b4b7bc3a439e0f17e9918cd647ecb85b101c
Limonchegggg <80949663+Limonchegggg@users.noreply.github.com>
2022-04-22 07:10:45 +0300

    Давно надо было

--- Bot/src/main/java/bot/WhiteListBot.java
+++ Bot/src/main/java/bot/WhiteListBot.java
@@ -1,10 +1,13 @@
 package bot;
 
+import org.bukkit.Bukkit;
+
 import bot.commands.Commands;
 import bot.commands.WhiteListJoin;
 import configs.Players;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 
 public class WhiteListBot{
 	private JDA jda;
@@ -18,6 +21,7 @@ public class WhiteListBot{
 			this.jda = JDABuilder.createDefault(TOKEN).build();
 			jda.addEventListener(new Commands());
 			jda.addEventListener(new WhiteListJoin());
+			jda.getPresence().setActivity(Activity.watching(Bukkit.getOnlinePlayers() + "/" + Bukkit.getMaxPlayers()));
 			System.out.println("Bot has started!");
 		}catch(Exception e) {
 			e.printStackTrace();
